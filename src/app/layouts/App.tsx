40d65fc891c451718ff9b697201ebfe3f4061d6a
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-06-09 17:15:48 +0700

    login form

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -6,15 +6,20 @@ import BookDetails from "../../features/books/BookDetails";
 import Books from "../../features/books/Books";
 
 import "flowbite";
+import Footer from "./Footer";
+import Cart from "../../features/cart/Cart";
+import { useState } from "react";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/books" component={Books} />
       <Route path="/book-detail/:id" component={BookDetails} />
-    </>
+      <Route exact path="/my-cart" component={Cart} />
+      <Footer />
+    </div>
   );
 }
 
